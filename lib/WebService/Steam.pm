d5e70ef0d7a4e1782becaf3e865768267c5b6097
James Raspass <jraspass@gmail.com>
2012-07-08 11:15:42 +0100

    now with makefile

---
+++ lib/WebService/Steam.pm
@@ -0,0 +1,20 @@
+package WebService::Steam;
+
+use strict;
+use warnings;
+
+our $VERSION = 0.1;
+
+1;
+
+__END__
+ 
+=head1 NAME
+
+WebService::Steam - An OO Perl interface to the Steam community data
+
+=head1 MODULES AND UTILITIES
+
+=head2 WebService::Steam::Group
+
+=head2 WebService::Steam::User
